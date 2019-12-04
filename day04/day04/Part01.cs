3876c83f5a590dd81335c0a368bd44e68a60984b
slemsvamp <silverljus@gmail.com>
2019-12-04 07:12:57 +0100

    Day04 WIP.

---
+++ day04/day04/Part01.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day04
+{
+    public class Part01
+    {
+        public string Run(string input)
+        {
+            // 1661 not right
+            // 278 not right
+            // 109 not right
+            var result = "";
+
+            var split = input.Split(new string[] { "-" }, StringSplitOptions.None);
+            var min = int.Parse(split[0]);
+            var max = int.Parse(split[1]);
+
+            int skipped = 0;
+            int valid = 0;
+
+            for (int n = min; n <= max; n++)
+            {
+                string s = n.ToString();
+
+                int d = 0;
+                int dec = 0;
+                for (int si = 0; si < s.Length - 1; si++)
+                {
+                    if (s[si] == s[si + 1]) d++;
+                    if (int.Parse(s[si].ToString()) > int.Parse(s[si + 1].ToString())) dec++;
+                }
+                if (d == 0 || dec > 0)
+                {
+                    skipped++;
+                    continue;
+                }
+                else
+                {
+                    valid++;
+                }
+            }
+
+            return (valid).ToString();
+        }
+    }
+}
---
+++ day04/day04/Part02.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day04
+{
+    public class Part02
+    {
+        public string Run(string input)
+        {
+            // 760 is not right
+            // 497 is not right
+            // 639 is not right
+
+            var z = Grouped("133345");
+
+            var split = input.Split(new string[] { "-" }, StringSplitOptions.None);
+            var min = int.Parse(split[0]);
+            var max = int.Parse(split[1]);
+
+            int skipped = 0;
+            int valid = 0;
+
+            for (int n = min; n <= max; n++)
+            {
+                string s = n.ToString();
+
+                int g = 0;
+                int d = 0;
+                int dec = 0;
+
+                for (int si = 0; si < s.Length - 1; si++)
+                {
+                    if (s[si] == s[si + 1])
+                    {
+                        d++;
+                    }
+                    if (int.Parse(s[si].ToString()) > int.Parse(s[si + 1].ToString())) dec++;
+                }
+
+                if (d > 0)
+                {
+
+                }
+
+                if (d == 0 || dec > 0 || g > 0)
+                {
+                    skipped++;
+                    continue;
+                }
+                else
+                {
+                    if (Grouped("z" + s + "z"))
+                    {
+                        skipped++;
+                        continue;
+                    }
+
+                    valid++;
+                }
+            }
+
+            return (valid).ToString();
+        }
+
+        private static bool Grouped(string s)
+        {
+            var groupedIntegers = new Dictionary<int, int>();
+            for (int x = 0; x < s.Length; x++)
+            {
+                int theSame = 1;
+                for (int playhead = 1; x + playhead < s.Length; playhead++)
+                {
+                    if (s.Substring(x, 1) != s.Substring(x + playhead, 1))
+                    {
+                        break;
+                    }
+                    theSame++;
+                }
+
+                if (theSame >= 2)
+                {
+                    var key = int.Parse(s.Substring(x, 1));
+                    if (!groupedIntegers.ContainsKey(key))
+                    {
+                        groupedIntegers.Add(key, theSame);
+                    }
+                    else if (groupedIntegers[key] < theSame)
+                    {
+                        groupedIntegers[key] = theSame;
+                    }
+                }
+            }
+
+            int biggest = -1;
+            foreach (var kvp in groupedIntegers)
+            {
+                if (kvp.Key > biggest)
+                {
+                    biggest = kvp.Key;
+                }
+            }
+
+            if (biggest == -1 || (groupedIntegers[biggest] % 2) == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
